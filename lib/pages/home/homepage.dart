5dc4f927c06623fa6d2454f5426380ddb9343713
ShahinaEP <shahinaeasminpia860703@gmail.com>
2023-01-16 14:12:09 +0600

    using map

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:uiorus/const-class/const-value.dart';
-import 'package:uiorus/custom-widget/search-widget/list-card-widget/list_card_widget.dart';
+import 'package:uiorus/custom-widget/row-list-widget/row_list_widget.dart';
 import 'package:uiorus/custom-widget/search-widget/search_widget.dart';
 import 'package:uiorus/pages/home/search.dart';
 
+import '../../custom-widget/list-card-widget/list_card_widget.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -25,34 +18,34 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List savedInventory = [
     {
-      'image':'images/car1.png',
+      'image': 'images/car1.png',
       'carModel': '2020 Genesis G70',
       'subTitle': '3.3T',
-      'price':'47,370'
+      'price': '47,370'
     },
     {
-      'image':'images/car2.png',
+      'image': 'images/car2.png',
       'carModel': '2019 Ford Edge',
       'subTitle': 'Titanium',
-      'price':'48,370'
+      'price': '48,370'
     },
     {
-      'image':'images/car1.png',
+      'image': 'images/car1.png',
       'carModel': 'BMW 3 Series for Sale Near 91204',
       'subTitle': '330i, Sedan',
-      'price':'67,309'
+      'price': '67,309'
     },
     {
-      'image':'images/car2.png',
+      'image': 'images/car2.png',
       'carModel': 'Mazda 3 for Sale Near 91204',
       'subTitle': 'Deep Crystal Blue Mica, Sedan',
-      'price':'57,370'
+      'price': '57,370'
     },
     {
-      'image':'images/car1.png',
+      'image': 'images/car1.png',
       'carModel': '2020 Genesis G70',
       'subTitle': '3.3T',
-      'price':'47,370'
+      'price': '47,370'
     },
   ];
 
@@ -62,14 +55,24 @@ class _MyHomePageState extends State<MyHomePage> {
 // print(savedInventory);
     return SafeArea(
       // This trailing comma makes auto-formatting nicer for build methods.
-      child: Column(
-        children: [
-          search_widget(size, context),
-          listOfCard('Saved Inventory',Constants().color1 , Constants().iconColors),
-          const SizedBox(height: 20,),
-          listOfCard('Saved Searches',Constants().color2, Constants().iconColors2 ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            search_widget(size, context),
+            listOfCard(
+                'Saved Inventory', Constants().color1, Constants().iconColors),
+            const SizedBox(
+              height: 20,
+            ),
+            listOfCard(
+                'Saved Searches', Constants().color2, Constants().iconColors2),
 
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            rowListWidget(savedInventory, Constants().color1,'Favourite Car', Constants().iconColors),
+          ],
+        ),
       ),
     );
   }
